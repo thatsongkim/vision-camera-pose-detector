ca3b27ca2e6877033e46e4c65b80ca348cae1559
ThatSongKim <thatsongkim@gmail.com>
2023-09-18 22:30:34 +0900

    feat(android): integrate with MLKit

--- android/src/main/java/com/visioncameraposedetector/VisionCameraPoseDetectorModule.java
+++ android/src/main/java/com/visioncameraposedetector/VisionCameraPoseDetectorModule.java
@@ -1,32 +1,109 @@
 package com.visioncameraposedetector;
 
+import android.graphics.PointF;
+import android.media.Image;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
-import com.facebook.react.bridge.Promise;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.bridge.WritableNativeMap;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.pose.Pose;
+import com.google.mlkit.vision.pose.PoseDetection;
+import com.google.mlkit.vision.pose.accurate.AccuratePoseDetectorOptions;
+import com.google.mlkit.vision.pose.PoseDetector;
 
-@ReactModule(name = VisionCameraPoseDetectorModule.NAME)
-public class VisionCameraPoseDetectorModule extends ReactContextBaseJavaModule {
-  public static final String NAME = "VisionCameraPoseDetector";
+import com.google.mlkit.vision.pose.PoseLandmark;
+import com.mrousavy.camera.frameprocessor.Frame;
+import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin;
+import com.mrousavy.camera.parsers.Orientation;
 
-  public VisionCameraPoseDetectorModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-  }
+import java.util.Map;
 
+public class VisionCameraPoseDetectorModule extends FrameProcessorPlugin {
+  private final AccuratePoseDetectorOptions options =
+    new AccuratePoseDetectorOptions.Builder()
+      .setDetectorMode(AccuratePoseDetectorOptions.STREAM_MODE)
+      .build();
+
+  private final PoseDetector detector = PoseDetection.getClient(options);
+
+  @Nullable
   @Override
-  @NonNull
-  public String getName() {
-    return NAME;
-  }
+  public Object callback(@NonNull Frame frame, @Nullable Map<String, Object> arguments) {
+    try {
+      Image mediaImage = frame.getImage();
+      Log.e("Log", frame.getPixelFormat());
+      if (mediaImage != null) {
+        InputImage image = InputImage.fromMediaImage(mediaImage, Orientation.Companion.fromUnionValue(frame.getOrientation()).toDegrees());
+        Task<Pose> result = detector.process(image);
+        Pose pose = result.getResult();
 
+        if (pose == null) {
+          return null;
+        }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  @ReactMethod
-  public void multiply(double a, double b, Promise promise) {
-    promise.resolve(a * b);
+        String[] landMarkNames = {
+          "nose",
+          "leftEyeInner",
+          "leftEye",
+          "leftEyeOuter",
+          "rightEyeInner",
+          "rightEye",
+          "rightEyeOuter",
+          "leftEar",
+          "rightEar",
+          "leftMouth",
+          "rightMouth",
+          "leftShoulder",
+          "rightShoulder",
+          "leftElbow",
+          "rightElbow",
+          "leftWrist",
+          "rightWrist",
+          "leftPinky",
+          "rightPinky",
+          "leftIndex",
+          "rightIndex",
+          "leftThumb",
+          "rightThumb",
+          "leftHip",
+          "rightHip",
+          "leftKnee",
+          "rightKnee",
+          "leftAnkle",
+          "rightAnkle",
+          "leftHeel",
+          "rightHeel",
+          "leftFootIndex",
+          "rightFootIndex"
+        };
+
+        WritableNativeMap landmarks = new WritableNativeMap();
+        for (var i = 0; i < landMarkNames.length; i++) {
+          PoseLandmark landmark = pose.getPoseLandmark(i);
+          WritableNativeMap map = new WritableNativeMap();
+          if (landmark == null) {
+            map.putDouble("x", 0);
+            map.putDouble("y", 0);
+            map.putDouble("confidence", 0);
+            landmarks.putMap(landMarkNames[i], map);
+            continue;
+          }
+          PointF positions = landmark.getPosition();
+          map.putDouble("x", positions.x);
+          map.putDouble("y", positions.y);
+          map.putDouble("confidence", landmark.getInFrameLikelihood());
+          landmarks.putMap(landMarkNames[i], map);
+        }
+
+        return landmarks;
+      }
+    } catch (Exception e) {
+      Log.e("Error", "Error processing pose detection", e);
+    }
+    return null;
   }
 }
