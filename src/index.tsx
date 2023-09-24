a753fca6e99fd122eabee5241b4a98e8ffb1abb8
ThatSongKim <thatsongkim@gmail.com>
2023-09-24 10:57:15 +0900

    feat: add types

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,50 @@
 import { VisionCameraProxy, type Frame } from 'react-native-vision-camera';
 
 const _detectPose = VisionCameraProxy.getFrameProcessorPlugin('detectPose');
-export function detectPose(frame: Frame): any {
+
+type Landmark = {
+  x: number;
+  y: number;
+  confidence: number;
+};
+
+export type Landmarks = {
+  nose: Landmark;
+  leftEyeInner: Landmark;
+  leftEye: Landmark;
+  leftEyeOuter: Landmark;
+  rightEyeInner: Landmark;
+  rightEye: Landmark;
+  rightEyeOuter: Landmark;
+  leftEar: Landmark;
+  rightEar: Landmark;
+  mouthLeft: Landmark;
+  mouthRight: Landmark;
+  leftShoulder: Landmark;
+  rightShoulder: Landmark;
+  leftElbow: Landmark;
+  rightElbow: Landmark;
+  leftWrist: Landmark;
+  rightWrist: Landmark;
+  leftPinkyFinger: Landmark;
+  rightPinkyFinger: Landmark;
+  leftIndexFinger: Landmark;
+  rightIndexFinger: Landmark;
+  leftThumb: Landmark;
+  rightThumb: Landmark;
+  leftHip: Landmark;
+  rightHip: Landmark;
+  leftKnee: Landmark;
+  rightKnee: Landmark;
+  leftAnkle: Landmark;
+  rightAnkle: Landmark;
+  leftHeel: Landmark;
+  rightHeel: Landmark;
+  leftFootIndex: Landmark;
+  rightFootIndex: Landmark;
+};
+
+export function detectPose(frame: Frame): Landmarks | undefined {
   'worklet';
   // @ts-ignore
   return _detectPose?.call(frame);
